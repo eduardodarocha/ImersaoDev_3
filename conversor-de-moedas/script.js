2e4c88274e3889ca2f4b7b0d3c9f60c1d9893e68
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-09-15 00:56:34 -0300

    Update conversor de moedas v2

---
+++ conversor-de-moedas/script.js
@@ -0,0 +1,7 @@
+﻿function converter() {
+    var valueDolar = document.getElementById("valueinput").value;
+    var valueReal = (parseFloat(valueDolar) * 5.5).toFixed(2);
+  
+    document.getElementById("valueconverted").innerHTML =
+      "R$" + valueReal.replace(".", ",");
+  }  
---
+++ conversor-de-moedas_v2/script/script.js
@@ -0,0 +1,46 @@
+﻿function converte() {
+    moeda = document.getElementById("currencyinput");
+    moedaInput = moeda.value;
+    console.log(moedaInput);
+    if (moedaInput == "USD") {
+        var url = "https://economia.awesomeapi.com.br/last/USD-BRL";
+        // var endpoint = "USD-BRL";
+        var moedaSimbolo = "USD";
+    } else if (moedaInput == "EUR") {
+        var url = "https://economia.awesomeapi.com.br/last/EUR-BRL";
+        // var endpoint = "EUR-BRL";
+        var moedaSimbolo = "EUR";
+    } else {
+        var url = "https://www.mercadobitcoin.net/api/BTC/ticker/";
+        // var endpoint = "BTC-BRL";
+        var moedaSimbolo = "BTC";
+    }
+    // const url = `https://economia.awesomeapi.com.br/last/${endpoint}`;
+
+    let casaDec = 2;
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            if (moedaInput == "USD") {
+                var valordata = data.USDBRL.bid;
+            } else if (moedaInput == "EUR") {
+                var valordata = data.EURBRL.bid;
+            } else {
+                var valordata = data.ticker.buy;
+                // var valordata = data.BTCBRL.bid;
+                casaDec = 10;
+            }
+
+            var valor = parseFloat(valordata);
+            console.log("Valor da moeda: " + valor + " " + moedaSimbolo);
+
+            let quantReal = document.getElementById("valueinput");
+            let real = quantReal.value;
+            let valueReal = (parseFloat(real) / valor).toFixed(casaDec);
+
+            document.getElementById("valueconverted").innerHTML = `O valor convertido de Real para ${moedaSimbolo} é: ${valueReal.replace(".", ",")}`;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
