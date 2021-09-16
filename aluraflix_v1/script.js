f214d4888ccee881f6bad57c3b51fd7368c38ea0
Eduardo Rocha <eduardomfrocha@gmail.com>
2021-09-16 19:58:26 -0300

    Update Aluraflix_v1

---
+++ aluraflix_v1/script.js
@@ -0,0 +1,20 @@
+var listaFilmes = [
+  "https://m.media-amazon.com/images/M/MV5BMTM0OTgzMjg2MF5BMl5BanBnXkFtZTcwMDg5MTUyMQ@@._V1_FMjpg_UY1000_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTExMzU0ODcxNDheQTJeQWpwZ15BbWU4MDE1OTI4MzAy._V1_FMjpg_UX200_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMjQ0NTI0NjkyN15BMl5BanBnXkFtZTgwNzY0MTE0NzM@._V1_FMjpg_UX200_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_FMjpg_UX1000_.jpg",
+  "https://i0.wp.com/www.aoseugosto.com/wp-content/uploads/2021/09/Rick-and-Morty-1a-Temporada-Completa-2013-Dublado-Torrent-Download.jpg",
+  "https://i1.wp.com/www.aoseugosto.com/wp-content/uploads/2020/12/Demon-Slayer-Kimetsu-no-Yaiba-O-filme-O-Trem-Infinito-Legendado-HDTV-TS-1080p-MKV.jpeg",
+  "https://i2.wp.com/www.aoseugosto.com/wp-content/uploads/2021/08/O-Esquadrao-Suicida-2021-Google-Drive-e-Torrent-Dublado-Dual-Audio-5.1-720p-4K-1080p-MKV-MP4.jpg",
+  "https://i2.wp.com/www.aoseugosto.com/wp-content/uploads/2021/09/Shang-Chi-e-a-Lenda-dos-Dez-Aneis-CAM-2021-Dublado-Torrent-Download.jpg", "https://preview.redd.it/b4xp6xhg4zg71.jpg?width=640&height=948&crop=smart&auto=webp&s=9e15cfeed7c77c0670bfbb1c253a17ec7fe625ce",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5ac2a737-7b8b-4a38-9bc6-8a89633a9f49/deelkz6-a9156874-f42f-4e7c-b32b-5cafd6e7b48f.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzVhYzJhNzM3LTdiOGItNGEzOC05YmM2LThhODk2MzNhOWY0OVwvZGVlbGt6Ni1hOTE1Njg3NC1mNDJmLTRlN2MtYjMyYi01Y2FmZDZlN2I0OGYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9JX7xTW1b1SCI_Nq0NQja2mkoDA1s-JDMEZAubnilfM", "https://br.web.img3.acsta.net/pictures/20/02/03/15/22/4954456.jpg", "https://m.media-amazon.com/images/M/MV5BNzA4MjEzMTktZWMxYS00YWQ1LWJhMWItZTUyMGFmZjFmMjE4XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg"
+];
+// listaFilmes.push("Harry Potter");
+// document.write("<p>" + listaFilmes[0] + "</p>");
+// document.write("<p>" + listaFilmes[1] + "</p>");
+// document.write("<p>" + listaFilmes[2] + "</p>");
+// document.write("<p>" + listaFilmes[3] + "</p>");
+
+for (let i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + " class='banners'>");
+}
